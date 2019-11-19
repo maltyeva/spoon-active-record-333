e01fcf296fbcf54cb8e4ea9e04e67f1a55fb5990
Maria <maria.altyeva@gmail.com>
2019-11-19 09:44:51 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #CRUD
+  #Read
+  # #Read all restaurants
+  # get '/restaurants', to: "restaurants#index"
+
+  # #Read one restaurant
+  # #restaurants/6
+  # get '/restaurants/:id', to: "restaurants#show"
+
+  # #Create one restaurant
+  # #1st reguest get the form
+  # get '/restaurants/new', to: "restaurants#new"
+
+  # #2nd request to submit the form
+  # post '/restaurants', to: "restaurants#create"
+
+  # #Update one restaurant
+  # #request to get the form
+  # #restaurants/6/edit
+  # get 'restaurants/:id/edit', to: "restaurants#edit"
+
+  # #request to submit the form
+  # patch 'restaurants/:id', to: "restaurants#update"
+
+  # # Destroy/Delete one restaurant
+  # #restaurants/6
+  # delete 'restaurants/:id', to: "restaurants#destroy"
+
+  resources :restaurants, except: [:destroy, :show]
+
 end
